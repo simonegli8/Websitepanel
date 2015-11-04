1811d3e637cd55386ef890948b4c46aac577ed72
Simon Egli <simon@johnshope.com>
2015-11-04 23:08:38 +0100

    Linux Support Introduced autogenerating WebsitePanel.Server versions for Linux and Mac that run on Mono without WSE. Server Versions for Mono without WSE can be distinguished by new Methods in AutoDiscovery service.
    Not yet implemented:
    WebsitePanel.Server.Client remains to be implemented for those server variants.

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/Common/AutoDiscoveryAttributes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebsitePanel.Providers {
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class NoAutodiscoveryAttribute : Attribute { }
+	[AttributeUsage(AttributeTargets.Class)]
+	public class RemoteProxyAttribute : Attribute { }
+
+}
